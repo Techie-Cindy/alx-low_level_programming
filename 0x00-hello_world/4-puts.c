4ecf211c146dc3f648d329d7d45719bc026cff30
Techie-Cindy <preciousmoronke@gmail.com>
2023-09-07 18:55:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this describes the main function of the code
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
