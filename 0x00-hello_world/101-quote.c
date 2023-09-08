ef91bf48775aa238cfaabf6518f45effa05b3092
Techie-Cindy <preciousmoronke@gmail.com>
2023-09-07 19:58:50 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <unistd.h>
+/**
+ * main - this prints exactly the input on it
+ * Return 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
