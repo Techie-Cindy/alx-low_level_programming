b1c116d42437e717afc75b170fe4f99f90b7b362
Techie-Cindy <preciousmoronke@gmail.com>
2023-10-01 18:23:35 -0700

    cindy

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,11 @@
 int _strlen_recursion(char *s)
 {
 	 int lnt = 0;
-	 
-	 if (*s)
-	 {
-		 lnt++;
-		 lnt += _strlen_recursion(s + 1);
-	 }
-	 return (lnt);
+
+	if (*s)
+	{
+		lnt++;
+		lnt += _strlen_recursion(s + 1);
+	}
+	return (lnt);
 }
